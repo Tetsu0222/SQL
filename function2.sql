f30696e5e465bb00b2fdc774263b882bb37d2c0f
tetsu <root2120@gmail.com>
2023-07-12 07:54:14 +0900

    文字ファンクション

---
+++ function2.sql
@@ -0,0 +1,23 @@
+//文字ファンクション
+//文字を戻すファンクション
+
+
+//UPPER
+SELECT UPPER(ename)
+FROM emp;
+
+→文字列を大文字に変換する。
+
+
+//LOWER
+SELECT LOWER(ename)
+FROM emp;
+
+→文字列を小文字に変換する。
+
+
+//INICAP
+SELECT INICAP(ename)
+FROM emp;
+
+→文字列を各単語単位に、先頭の文字を大文字、2文字目以降の文字を小文字に変換する。

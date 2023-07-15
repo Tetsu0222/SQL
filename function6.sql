b572b2788cb170e03cc0a448b296e1c7805ff849
tetsu <root2120@gmail.com>
2023-07-16 08:13:18 +0900

    数値を返すファンクション

---
+++ function6.sql
@@ -0,0 +1,30 @@
+//数値ファンクション
+
+//ROUND
+SELECT ROUND( 18.617 )
+FROM dual;
+
+→数値を四捨五入する。
+
+
+//TRUNC
+SELECT TRUNC( 18.617 )
+FROM dual;
+
+→数値を切り捨てる。
+
+
+//MOD
+SELECT MOD( 9 , 4 )
+FROM dual;
+
+→数値を除算した時の余りを求める。
+
+
+//POWER
+SELECT POWER( 2 , 2 )
+FROM dual;
+
+→数値のべき乗を求める。
+
+

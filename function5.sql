cfb292f7363adce16804c4037b5ddd0435fa47eb
tetsu <root2120@gmail.com>
2023-07-15 09:56:04 +0900

    数値を返すファンクション

---
+++ function5.sql
@@ -0,0 +1,14 @@
+//数字を戻す文字ファンクション
+
+//LENGTH
+SELECT LENGTH( '1234567890' )
+FROM dual;
+
+→文字数を数値で返す。
+
+//INSTAR
+SELECT INSTAR( 'ABCDEFG' , 'C' )
+FROM dual;
+
+→第2引数で指定した文字が、第1引数で指定されている文字の中で何番目に位置しているか数値で返す。
+

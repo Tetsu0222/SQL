9689290800b067ca7687aed7a6f490c36eacd40e
tetsu <root2120@gmail.com>
2023-07-24 08:05:13 +0900

    集計ファンクション

--- function11.sql
+++ function11.sql
@@ -7,3 +7,31 @@ FROM emp;
 
 →SAL列の合計を求める。
 
+
+
+//AVG
+SELECT AVG( sal )
+FROM emp;
+
+→SAL列の平均を求める。
+
+
+//MAX
+SELECT MAX( sal )
+FROM emp;
+
+→SAL列の最大値を求める。
+
+
+//MIN
+SELECT MIN( sal )
+FROM emp;
+
+→SAL列の最小値を求める。
+
+
+//COUNT
+SELECT COUNT( sal )
+FROM emp;
+
+→SAL列の列数を数える。

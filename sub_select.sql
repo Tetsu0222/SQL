8a0689f89830e676c16538f9815a3b6a0aa56c99
tetsu <root2120@gmail.com>
2023-07-27 07:59:58 +0900

    サブクエリ2

--- sub_select.sql
+++ sub_select.sql
@@ -7,3 +7,12 @@ WHERE sal > ( SELECT AVG( sal ) FROM emp );
 
 →1つの行における1つの列の値を戻す。
 例であれば、給与額が平均より上の社員をリストアップする。
+
+
+//HAVING句でのスカラー副問い合わせ
+SELECT deptno , AVG( sal )
+FROM emp
+GROUP BY deptno
+HAVING AVG( sal ) < ( SELECT AVG( sal ) FROM emp );
+
+→全社員の平均給与より、平均給与が低い部署を抽出

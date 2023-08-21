bc7a7c4d8e0bccb9128fe2ad481dd64e5b1440cb
tetsu <root2120@gmail.com>
2023-08-21 18:05:20 +0900

    条件付きインサート

--- multi_insert.sql
+++ multi_insert.sql
@@ -12,3 +12,12 @@ select n , c1 , c2 from t0;
 →複数のINTO句を指定できる。
 →VALUES句にサブクエリから戻される行の列名を指定できる。
 →サブクエリが戻す列数とターゲット表の列数が等しい場合、values句の指定を省略できる。
+
+
+
+//条件付きインサート
+insert all
+when n <= 1 then
+into t1 values( n , c1 )
+when n < 2 then
+into t2 values( n , c2 )
